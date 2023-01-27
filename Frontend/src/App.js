8f58770229aac1afce1861b66c66c607c9302dda
govind kumar <gk4051668@gmail.com>
2023-01-27 15:45:11 +0530

    added logo

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,16 @@
+import AllRoutes from "./AllRoutes/AllRoutes";
 import "./App.css";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRoutes />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,18 +5,12 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
-import { store } from "./Redux/store";
-import AuthContextProvider from "./Redux/Login_Auth";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
-    <Provider store={store}>
-      <AuthContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthContextProvider>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ChakraProvider>
 );
