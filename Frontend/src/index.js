fc5f67c54daf4f39d4fab3a9a18bf9b8027fd595
govind kumar <gk4051668@gmail.com>
2023-01-27 15:12:07 +0530

    added folder structure to frontend

---
+++ Frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
+import AuthContextProvider from "./Redux/Login_Auth";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ChakraProvider>
+    <Provider store={store}>
+      <AuthContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthContextProvider>
+    </Provider>
+  </ChakraProvider>
+);
