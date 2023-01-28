b6dd5f8678328eca3f48a0e564ac9efe8a52e670
ShravaniMishra <shravniguddi@gmail.com>
2023-01-29 01:06:45 +0530

    added code

--- Frontend/src/AllRoutes/AllRoutes.jsx
+++ Frontend/src/AllRoutes/AllRoutes.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import About from "../Components/About";
 import AllProducts from "../Pages/AllProductPage/AllProducts";
 import Appointment from "../Pages/Appointment/Appointment";
-<<<<<<< HEAD
-import About from "../Components/About";
-=======
 import Cart from "../Pages/Cart/Cart";
->>>>>>> f92d241ba7d53a529f5cc24e858533ce11620fe8
 import Home from "../Pages/Home";
 import SingleProduct from "../Pages/SingleProduct/SingleProduct";
 
@@ -15,14 +12,10 @@ const AllRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<AllProducts />} />
+      <Route path="/about" element={<About/>}/>
       <Route path="/products/:id" element={<SingleProduct />} />
       <Route path="/appointment" element={<Appointment />} />
-<<<<<<< HEAD
-      <Route path="/about" element={<About />} />
-      
-=======
       <Route path="/cart" element={<Cart />} />
->>>>>>> f92d241ba7d53a529f5cc24e858533ce11620fe8
     </Routes>
   );
 };
